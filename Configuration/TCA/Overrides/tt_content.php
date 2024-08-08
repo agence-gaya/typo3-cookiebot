58fe9dd8be811a816e558d7112a8c7a171f2c7c3
Benoit Chenu <bchenu@gaya.fr>
2024-08-08 16:32:06 +0200

    [TASK] The TCA field 'CType' of table 'tt_content' uses the legacy way of defining 'items'.

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 $GLOBALS['TCA']['tt_content']['types']['cookiebot_declaration'] = [
@@ -16,12 +17,16 @@ $GLOBALS['TCA']['tt_content']['types']['cookiebot_declaration'] = [
 $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['cookiebot_declaration'] = 'actions-cookie';
 
 // Adds the content element to the "Type" dropdown
+$item = [
+    'label' => 'LLL:EXT:cookiebot/Resources/Private/Language/backend.xlf:tt_content.cookiebot.title',
+    'value' => 'cookiebot_declaration',
+    'icon' => 'actions-cookie',
+];
+if ((new Typo3Version())->getMajorVersion() < 12) {
+    $item = array_values($item);
+}
 ExtensionManagementUtility::addTcaSelectItem(
     'tt_content',
     'CType',
-    [
-        'LLL:EXT:cookiebot/Resources/Private/Language/backend.xlf:tt_content.cookiebot.title',
-        'cookiebot_declaration',
-        'actions-cookie',
-    ]
+    $item
 );
